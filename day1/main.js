bf75c19b212036dac9854a01f0ad7361da10552e
SandipM03 <sandipmandal816708@gmail.com>
2024-09-16 21:22:41 +0530

    day 2 update

--- day1/main.js
+++ day1/main.js
@@ -1,51 +1,27 @@
-const minusbtn=document.querySelector('.minus')
-const plusbtn=document.querySelector(".plus")
-const value=document.querySelector('.value')
-const resetbtn=document.querySelector('.reset')
-const changeBy=document.querySelector('.changeBy')
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const changeBy = document.getElementById('changeBy');
+const reset = document.getElementById('reset');
+const value = document.querySelector('.value');
 
+increment.addEventListener('click', () => {
+  value.textContent = +value.textContent + changeBy.valueAsNumber;
+});
 
-minusbtn.addEventListener('click',()=>{
+decrement.addEventListener('click', () => {
+  value.textContent = parseInt(value.textContent) - parseInt(changeBy.value);
+});
 
-    const countValue=parseInt(value.innerText)
-    const changeByvalue=parseInt(changeBy.valueAsNumber)
-    value.innerText=countValue - changeByvalue;
-})
+changeBy.addEventListener('change', () => {
+  const changeByValue = changeBy.valueAsNumber;
 
-plusbtn.addEventListener('click',()=>{
-    const countValue=parseInt(value.innerText)
-    const changeByvalue=parseInt(changeBy.valueAsNumber)
-    value.innerText=countValue + changeByvalue;
-})
+  if (Number.isNaN(changeByValue)) {
+    changeBy.value = 1;
+  } else if (changeByValue < 0) {
+    changeBy.value = 1;
+  }
+});
 
-
-
-
-
-// const increment = document.getElementById('increment');
-// const decrement = document.getElementById('decrement');
-// const changeBy = document.getElementById('changeBy');
-// const reset = document.getElementById('reset');
-// const value = document.querySelector('.value');
-
-// increment.addEventListener('click', () => {
-//   value.textContent = +value.textContent + changeBy.valueAsNumber;
-// });
-
-// decrement.addEventListener('click', () => {
-//   value.textContent = parseInt(value.textContent) - parseInt(changeBy.value);
-// });
-
-// changeBy.addEventListener('change', () => {
-//   const changeByValue = changeBy.valueAsNumber;
-
-//   if (Number.isNaN(changeByValue)) {
-//     changeBy.value = 1;
-//   } else if (changeByValue < 0) {
-//     changeBy.value = 1;
-//   }
-// });
-
-// reset.addEventListener('click', () => {
-//   value.textContent = 0;
-// });
+reset.addEventListener('click', () => {
+  value.textContent = 0;
+});
